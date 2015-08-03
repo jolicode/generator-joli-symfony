01e03d4042a35bfc3a43489386b1e3c19094da3e
Laurent Brunet <lbrunet@jolicode.com>
2015-08-03 19:41:13 +0200

    Replace demo file **app.js** by **calculator.js**

---
+++ app/templates/demo/js/calculator.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var Calculator = {
+  add: function add(a, b) {
+    return console.log(a + b);
+  },
+  substract: function substract(a, b) {
+    return console.log(a - b);
+  }
+};
+
+module.exports = Calculator;
